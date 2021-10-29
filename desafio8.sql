f55293296cab225b15b241efe9c30846edc65bbd
Murilo Rainho <murilorainho01@gmail.com>
2021-10-29 19:06:33 -0300

    resolvendo erro req 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,9 +4,9 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM usuarios_artistas ua
+	DELETE FROM SpotifyClone..usuarios_artistas ua
     WHERE OLD.usuario_id = ua.usuario_id;
-    DELETE FROM historico_de_reproducoes hr
+    DELETE FROM SpotifyClone.historico_de_reproducoes hr
     WHERE OLD.usuario_id = hr.usuario_id;
 END $$;
 
